212f6853725fa864831f17b878d0807222b3fcbc
bu0532 <alexis.blancfeuillet@gmail.com>
2024-06-19 15:58:52 +0200

    lab6_2

--- examples/test.pm
+++ examples/test.pm
@@ -1,65 +1,77 @@
 Program Suspension;
-monitor PC;
-const nmrProcesses = 20;
-var i: INTEGER;
-delayed: array[0..nmrProcesses] of boolean;
-time: array[0..nmrProcesses] of integer;
-cond: array[0..nmrProcesses] of CONDITION;
-procedure oneSecondHasPassed;
-var i: INTEGER;
-begin
-for i:= 1 to nmrProcesses do
-IF delayed[i] then begin
-time[i]:= time[i] - 1;
-IF time[i] = 0 then begin
-delayed[i]:= false;
-SIGNALC (cond[i]);
-end
-end
-end;
-procedure delayMe (myself: INTEGER; intervalo: INTEGER);
-begin
-delayed [myself]:= TRUE;
-time [myself]:= intervalo;
-WAITC (cond[myself]);
-end;
-begin (* del monitor *)
-for i:= 1 to nmrProcesses do begin
-delayed[i]:= false;
-time[i]:= 0;
-end;
-end;
-const nmrProcesses = 20;
-var i: INTEGER;
-currentTime: INTEGER:= 0;
-output: BINARYSEM := 1;
-procedure reloj;
-var i, j: integer;
-begin
-for i:= 1 to 10 do begin
-for j:= 1 to 200 do;
-currentTime:= currentTime + 1;
-WAIT (output);
-writeln ("1 second has passed. Current time: “, currentTime,” seconds");
-SIGNAL (output);
-oneSecondHasPassed;
-end;
-end;
-procedure process (ID: integer);
-var I, J: integer;
-begin
-WAIT(output);
-writeln ("I’m the process", ID, " and I’m going to sleep ", ID*2, " seconds. Current time: ",
-currentTime);
-SIGNAL (output);
-delayMe (ID, ID*2);
-WAIT (output);
-writeln ("soy el process", ID, "and I’ve woken up. Current time: ", currentTime);
-SIGNAL (output);
-end;
-begin
-cobegin
-reloj;
-process(1); process (2); process (3); process (4);
-coend;
-end.
+MONITOR PC;
+CONST 
+    nmrProcesos = 20;
+VAR 
+    i: INTEGER;
+    delayed: array[0..nmrProcesos] of BOOLEAN;
+    time: array[0..nmrProcesos] of INTEGER;
+    cond: array[0..nmrProcesos] of CONDITION;
+
+PROCEDURE oneSecondHasPassed;
+VAR 
+    i: INTEGER;
+BEGIN
+    FOR i := 0 TO nmrProcesos DO
+        IF delayed[i] THEN BEGIN
+            time[i] := time[i] - 1;
+            IF time[i] = 0 THEN BEGIN
+                delayed[i] := false;
+                SIGNALC(cond[i]);
+            END;
+        END;
+END;
+
+PROCEDURE delayMe(myself: INTEGER; interval: INTEGER);
+BEGIN
+    delayed[myself] := true;
+    time[myself] := interval;
+    WAITC(cond[myself]);
+END;
+
+BEGIN (* monitor PC *)
+    FOR i := 0 TO nmrProcesos DO BEGIN
+        delayed[i] := false;
+        time[i] := 0;
+    END;
+END;
+
+VAR 
+    i: INTEGER;
+    currentTime: INTEGER := 0;
+    output: BINARYSEM := 1;
+
+PROCEDURE clock;
+VAR 
+    i, j: INTEGER;
+BEGIN
+    FOR i := 1 TO 10 DO BEGIN
+        FOR j := 1 TO 200 DO BEGIN
+            currentTime := currentTime + 1;
+            WAIT(output);
+            writeln('1 second passed. CurrentTime: ', currentTime, ' seconds.');
+            SIGNAL(output);
+        END;  (* Fin de la boucle j *)
+    END;  (* Fin de la boucle i *)
+END;  (* Fin de la procédure clock *)
+
+PROCEDURE process(ID: INTEGER);
+BEGIN
+    WAIT(output);
+    writeln('I am the process ', ID, ' and I go to sleep for ', ID*2, ' seconds. Current time: ', currentTime);
+    SIGNAL(output);
+    delayMe(ID, ID * 2);
+    WAIT(output);
+    writeln('I am the process ', ID, ' and I just woke up. Current time: ', currentTime);
+    SIGNAL(output);
+END;
+
+BEGIN
+    COBEGIN
+        clock;
+        process(1);
+        process(2);
+        process(3);
+        process(4);
+    COEND;
+END.
